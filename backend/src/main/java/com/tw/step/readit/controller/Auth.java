53ea7cb8e13645f7fbadb3c7106ecf3c43b7cb50
Dinesh Kumar <179027812+dineshkumar8700@users.noreply.github.com>
2026-05-17 10:14:23 +0530

    Adds global cors config file in a config package

--- backend/src/main/java/com/tw/step/readit/controller/Auth.java
+++ backend/src/main/java/com/tw/step/readit/controller/Auth.java
@@ -3,12 +3,10 @@ package com.tw.step.readit.controller;
 import com.tw.step.readit.model.loggedInResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class Auth {
 
     @GetMapping("/is-logged-in")
